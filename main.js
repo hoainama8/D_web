3fb3a4e604e4a328c52d2e35e38460b9a2456c25
hoainama8 <hoainama8@gmail.com>
2021-11-12 09:04:01 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-var 
+var videoNode = document.querySelector('#video');
+var videotag = videoNode.getElementsByTagName('video');
+var sourceVideo = videoNode.getElementsByTagName('source')
+
+var defaultVideo = videoNode.lastElementChild.src;
+defaultVideo = 'https://siasky.net/AAAe_ifd8iWaCevMesvI9xWqvcOpsM09KWWnCXWkjU_hog'
+console.log(videoNode.lastElementChild.src, videotag, sourceVideo.attributes)
