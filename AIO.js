bd1f68a0e13c51cada57210e88b5cbc57a834782
hoainama8 <hoainama8@gmail.com>
2022-03-14 09:22:22 +0700

    Create AIO.js

---
+++ AIO.js
@@ -0,0 +1,66 @@
+    var Bullwinkle = 'https://siasky.net/AABH9EGLpzPA1cVZbL_yYpk58lqnFhzJYdtobuc7XMpZbQ';
+    const listFim = [
+        {
+            id: '1',
+            name: 'thuong nho nguoi dung',
+            category: 'hanh dong',
+            national: 'viet nam',
+            year: '2011',
+            pic: Bullwinkle,
+            link: ''
+        },
+        {
+            id: '2',
+            name: 'Dung quat em',
+            category: 'tinh cam',
+            national: 'han quoc',
+            year: '2012',
+            pic: Bullwinkle,
+            link: '',
+        },
+        {
+            id: '3',
+            name: 'thuong nho nguoi dung',
+            category: 'hanh dong',
+            national: 'viet nam',
+            year: '2011',
+            pic: Bullwinkle,
+            link: ''
+        },
+        {
+            id: '4',
+            name: 'thuong nho nguoi dung',
+            category: 'hanh dong',
+            national: 'viet nam',
+            year: '2011',
+            pic: Bullwinkle,
+            link: ''
+        },
+        {
+            id: '5',
+            name: 'thuong nho nguoi dung',
+            category: 'hanh dong',
+            national: 'viet nam',
+            year: '2011',
+            pic: Bullwinkle,
+            link: ''
+        },
+        {
+            id: '6',
+            name: 'thuong nho nguoi dung',
+            category: 'hanh dong',
+            national: 'viet nam',
+            year: '2011',
+            pic: Bullwinkle,
+            link: ''
+        },
+        {
+            id: '7',
+            name: 'thuong nho nguoi dung',
+            category: 'hanh dong',
+            national: 'viet nam',
+            year: '2011',
+            pic: './src/component/image/Bullwinkle.png',
+            link: ''
+        },
+    ];
