ae6cc923b22c0df7a8c3d0012d028a3b84debbe2
hoainama8 <hoainama8@gmail.com>
2022-04-30 11:28:30 +0700

    Update class.js

--- class.js
+++ class.js
@@ -3,5 +3,6 @@ const gray = {
   background-color: "gray"
 }
  const btn = document.createElement('button')
+  btn.textContent = "Gray"
     btn.classList.add('btn gray')
     btnNode.appendChild('btn')
