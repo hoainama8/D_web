6d073145b7eee33f7cecbf6fddc436fa4bbe53fa
hoainama8 <hoainama8@gmail.com>
2022-05-02 11:37:57 +0700

    Update class.js

--- class.js
+++ class.js
@@ -1,4 +1,11 @@
 const btncreate = document.createElement('button');
     btncreate.innerText = "gray";
     btncreate.classList.add('btn');
+ btncreate.classList.add('gray');
     document.getElementById("changeColor").appendChild(btncreate);
+
+const btnBlue = document.createElement('button');
+    btnBlue.innerText = "Blue";
+    btnBlue.classList.add('btn');
+    btnBlue.classList.add('blue');
+    document.getElementById("changeColor").appendChild(btnBlue);
