2e33d1f0f5022cba622da82c4713dac60266fcbb
hoainama8 <hoainama8@gmail.com>
2022-05-02 10:37:04 +0700

    Update class.js

--- class.js
+++ class.js
@@ -1,4 +1,4 @@
 const btncreate = document.createElement('button')
     btncreate.innerText = "gray"
     btncreate.classList.add('btn');
-    document.getElementById("changeColor").appendChild(btncreate)
+    document.getElementsByClassName("changeColor").appendChild(btncreate)
